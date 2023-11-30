1bb890a5caec927f5535334c8e56dcfa0902599f
Juan Antamba <juan.antamba@udla.edu.ec>
2023-11-30 16:30:03 -0500

    Se inicializa el arreglo tridimensional en 0, y ediante bucles for se inicializa la ultma matriz en unos

--- ArregloTridimensional.c
+++ ArregloTridimensional.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-
 int main() {
     int num_matrices, filas, columnas;
+
     printf("Ingrese el numero de matrices: ");
     scanf("%d", &num_matrices);
     printf("Ingrese el numero de filas: ");
@@ -9,5 +9,14 @@ int main() {
     printf("Ingrese el numero de columnas: ");
     scanf("%d", &columnas);
 //En esta parte se pide al usuario las dimensiones de las matrices y el numero de las mismas, ademas de declarar las variables
+    
+    int arreglo_tridimensional[num_matrices][filas][columnas]; //en este apartado se inicializa el arreglo tridimensional en ceros
+
+    for (int i = 0; i < filas; i++) {
+        for (int j = 0; j < columnas; j++) {
+            arreglo_tridimensional[num_matrices - 1][i][j] = 1;
+        }
+    }
+    //En este bucle se inicializa la ultima matriz en unos 
 }
 
