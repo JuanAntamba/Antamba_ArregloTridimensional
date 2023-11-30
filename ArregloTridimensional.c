007bd06fc77d33af2ea436db0c08916e38be4dae
Juan Antamba <juan.antamba@udla.edu.ec>
2023-11-30 16:25:01 -0500

    Se declaran las variables para la creacion de dimension y numeros de matrices, esta informacion se pide al usuaio

---
+++ ArregloTridimensional.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int num_matrices, filas, columnas;
+    printf("Ingrese el numero de matrices: ");
+    scanf("%d", &num_matrices);
+    printf("Ingrese el numero de filas: ");
+    scanf("%d", &filas);
+    printf("Ingrese el numero de columnas: ");
+    scanf("%d", &columnas);
+//En esta parte se pide al usuario las dimensiones de las matrices y el numero de las mismas, ademas de declarar las variables
+}
+
